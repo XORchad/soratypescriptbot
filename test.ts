ec50d9ad9765963655206e81f1ea66ed616b2182
XORChad <xorchad@gmail.com>
2023-11-07 23:41:27 +0000

    add try-catch-finnaly to make sure connection is closed each time

--- test.ts
+++ test.ts
@@ -1,35 +1,40 @@
 import { FPNumber, api } from '@sora-substrate/util';
 import { connection } from '@sora-substrate/connection';
-import { SORA_ENV } from '@sora-substrate/types/scripts/consts';
 
 const MNEMONIC = 'friend shuffle alien sniff maid dog tomorrow face sponsor place man quantum';
 
 async function getQuote(): Promise<void> {
-    const env = SORA_ENV.prod;
-    // const env = 'wss://ws.mof.sora.org';
+    const env = 'wss://ws.mof.sora.org';
 
     await connection.open(env);
     console.info('Connected: ' + env);
 
-    const withKeyringLoading = true;
-    await api.initialize(withKeyringLoading);
+    try {
 
-    api.importAccount(MNEMONIC, 'name', 'pass');
+        const withKeyringLoading = true;
+        await api.initialize(withKeyringLoading);
 
-    const amount_in = 100;
-    const res = await api.swap.getResultRpc(
-        '0x0200060000000000000000000000000000000000000000000000000000000000', 
-        '0x0200000000000000000000000000000000000000000000000000000000000000', 
-        amount_in
-    );
+        api.importAccount(MNEMONIC, 'name', 'pass');
 
-    const amount = FPNumber.fromCodecValue(res.amount);
-    const amount_quote = amount.toLocaleString();
-    console.info(`[amount: ${amount_quote} XOR, for: ${amount_in} DAI`);
+        const amount_in = 100;
+        const res = await api.swap.getResultRpc(
+            '0x0200060000000000000000000000000000000000000000000000000000000000', 
+            '0x0200000000000000000000000000000000000000000000000000000000000000', 
+            amount_in
+        );
 
-    api.logout();
-    await connection.close();
-    console.info('Disconnected: ' + env);
+        const amount = FPNumber.fromCodecValue(res.amount);
+        const amount_quote = amount.toLocaleString();
+        console.info(`[amount: ${amount_quote} XOR, for: ${amount_in} DAI`);
+
+    } catch(error:unknown) {
+        console.log(error);
+    } finally {
+        api.logout();
+        await connection.close();
+        console.info('Disconnected: ' + env);
+    }
+
 };
   
 getQuote()
